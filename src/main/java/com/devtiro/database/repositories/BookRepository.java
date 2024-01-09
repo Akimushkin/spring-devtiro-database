4a58fd6ee47894f39da0628a6e20dddf9cd9a5e1
Sergey Akimushkin <akimushkin@outlook.com>
2024-01-09 23:29:39 +0300

    Read and Read Many implemented

--- src/main/java/com/devtiro/database/repositories/BookRepository.java
+++ src/main/java/com/devtiro/database/repositories/BookRepository.java
@@ -1,4 +1,9 @@
 package com.devtiro.database.repositories;
 
-public interface BookRepository {
+import com.devtiro.database.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, String> {
 }
