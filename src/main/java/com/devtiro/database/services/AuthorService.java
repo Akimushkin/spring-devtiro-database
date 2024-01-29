a11fd2bf7430ca5fdc6b182c5b822c1bb2a8498a
Sergey Akimushkin <akimushkin@outlook.com>
2024-01-29 12:08:20 +0300

    Implemented AuthorsList

--- src/main/java/com/devtiro/database/services/AuthorService.java
+++ src/main/java/com/devtiro/database/services/AuthorService.java
@@ -3,6 +3,10 @@ package com.devtiro.database.services;
 import com.devtiro.database.domain.entities.AuthorEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 public interface AuthorService {
     AuthorEntity createAuthor(AuthorEntity authorEntity);
+
+    List<AuthorEntity> findAll();
 }
